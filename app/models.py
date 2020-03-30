61099938c502e504aa5a67c781f6066f5f2af00f
Michal <mekalissa0@gmail.com>
2020-03-30 20:08:23 +0300

    fix ipn

--- app/models.py
+++ app/models.py
@@ -53,6 +53,26 @@ class Replies(db.Model):
         return f"Reply('{self.content}', '{self.timestamp}')"
 
 
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    payer_email = db.Column(db.String(100))
+    unix = db.Column(db.String(100))
+    payment_date = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    payment_gross = db.Column(db.Float(6,2))
+    payment_fee = db.Column(db.Float(6,2))
+    payment_net = db.Column(db.Float(6,2))
+    payment_status = db.Column(db.String(30))
+    txn_id = db.Column(db.String(100))
+
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Payment('{self.price}', '{self.title}')"
+        
+
+
 class Author(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     google_id = db.Column(db.String(100))
@@ -98,22 +118,7 @@ class Consultation(db.Model):
 
 
 
-class Payment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    payer_email = db.Column(db.String(100))
-    unix = db.Column(db.String(100))
-    payment_date = db.Column(db.String(100))
-    username = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
-    payment_gross = db.Column(db.Float(6,2))
-    payment_fee = db.Column(db.Float(6,2))
-    payment_net = db.Column(db.Float(6,2))
-    payment_status = db.Column(db.String(30))
-    txn_id = db.Column(db.String(100))
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
 
-    def __repr__(self):
-        return f"Payment('{self.price}', '{self.title}')"
 
 
 
